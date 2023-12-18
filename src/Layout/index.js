4c54d5de5c4f97b9923f643eba2dc8d7de7504d0
Max Deyo <mdeyo@indeed.com>
2023-12-18 14:53:15 -0800

    edits

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -11,12 +11,6 @@ import { StudyDeck } from "./StudyDeck";
 import { AddCard } from "./AddCard";
 import { EditCard } from "./EditCard";
 
-// Study	/decks/:deckId/study	Allows the user to study the cards from a specified deck
-// Create Deck	/decks/new	Allows the user to create a new deck
-// Deck	/decks/:deckId	Shows all of the information about a specified deck with options to edit or add cards to the deck, navigate to the study screen, or delete the deck
-// Edit Deck	/decks/:deckId/edit	Allows the user to modify information on an existing deck
-// Add Card	/decks/:deckId/cards/new	Allows the user to add a new card to an existing deck
-// Edit Card	/decks/:deckId/cards/:cardId/edit
 
 function Layout() {
   return (
